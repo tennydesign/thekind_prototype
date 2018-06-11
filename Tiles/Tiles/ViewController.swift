3603f0154ca0fed7887d7d46c30f349333f7bdfb
tennydesign <tenny@tennypinheiro.com>
2018-06-11 16:23:30 -0700

    Adding Tiles ptype

---
+++ Tiles/Tiles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tiles
+//
+//  Created by Tenny on 11/06/18.
+//  Copyright © 2018 imat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
