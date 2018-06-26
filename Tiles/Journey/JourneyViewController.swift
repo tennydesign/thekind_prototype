6372da1aa79c3bb0b9c93c7e80c0191b1a1a31ce
tennydesign <tenny@tennypinheiro.com>
2018-06-25 19:37:46 -0700

    Installing KOLODA pod

--- Tiles/Journey/JourneyViewController.swift
+++ Tiles/Journey/JourneyViewController.swift
@@ -7,22 +7,14 @@
 //
 
 import UIKit
-import SpriteKit
+
 
 class JourneyViewController: UIViewController {
 
-    @IBOutlet var journeySkView: SKView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        if let scene = SKScene(fileNamed: "JourneyScene") as? JourneyScene {
-            scene.scaleMode = .aspectFill
-            journeySkView.presentScene(scene)
-        } else {
-            print("scene not found")
-        }
-        journeySkView.ignoresSiblingOrder = true
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,14 +23,6 @@ class JourneyViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
